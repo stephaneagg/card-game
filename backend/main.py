afc2b496527362e65029812a435b511d4da6cecc
stephaneg <stephaneagg@gmail.com>
2025-12-08 20:09:01 -0800

    added cors middleware to backend

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,46 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    username: str
+    rank: int
+
+# list of users
+users = []
 
 @app.get("/")
 def root():
     return {"status": "backend running"}
+    #return {"players": 1, "president": "player1", "bum": "player1"}
 
 @app.get("/api/hello")
 def hello():
     return {"message": "Hello from FastAPI 🎉"}
+
+@app.get("/api/lobby")
+def players():
+    return {"players": users}
+
+
+
+
+
+
+
+
+# Allow React frontend access
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # Only let your frontend connect
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
--- frontend/card-game/src/App.tsx
+++ frontend/card-game/src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/ping")
+      .then(res => res.json())
+      .then(data => console.log("Backend says:", data))
+      .catch(err => console.error("Fetch error:", err));
+  }, []);
+
 
   return (
     <>
