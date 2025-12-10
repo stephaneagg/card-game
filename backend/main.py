c5eb52c7874548bb3aa20c0e26bb4a62dd67efc1
stephaneg <stephaneagg@gmail.com>
2025-12-09 22:35:32 -0800

    added backend logic to create players and lobbies

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,63 @@
+import uuid
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from typing import List
 
-app = FastAPI()
 
-class User(BaseModel):
+app = FastAPI(
+    title="Card game",
+    description="a game played with cards",
+    version="0.1.0"
+  #  docs_url="/docs"
+)
+
+class Player(BaseModel):
     username: str
-    rank: int
 
-# list of users
-users = []
+class Lobby(BaseModel):
+    players: List[Player]
+    winner: Player
+    loser: Player
+
+class RegisterReq():
+    name: str
+
+class LobbyReq():
+    player_id: int
+    lobby_id: int
+
+## MEMORY ##
+players = {}
+lobbies={}
+##
 
 @app.get("/")
 def root():
     return {"status": "backend running"}
     #return {"players": 1, "president": "player1", "bum": "player1"}
 
-@app.get("/api/hello")
-def hello():
-    return {"message": "Hello from FastAPI 🎉"}
+@app.post("/player/register")
+def register_player(data: RegisterReq):
+    player_id = str(uuid.uuid4())
+    players[player_id] = {
+        "name" : data.name
+    }
+    return {"player_id": player_id, "name:": RegisterReq.name}
 
-@app.get("/api/lobby")
-def players():
-    return {"players": users}
+@app.get("/players/")
+def get_players():
+    return players
 
 
+@app.post("/lobby/create")
+def create_lobby(data: LobbyReq):
+    lobby_id = str(uuid.uuid4())
+    lobbies[lobby_id] = {
+        players: [data.player_id],
+        "status": "waiting"
+    }
+    return {"lobby_id": lobby_id}
 
 
 
@@ -44,3 +77,4 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
